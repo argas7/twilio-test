9f095d761d18b884373dfaf043d1bb8b28537490
argas7 <argas@cin.ufpe.br>
2020-10-31 12:46:24 -0300

    Hotfix(readme): add readme and update others things

--- src/VideoChat.jsx
+++ src/VideoChat.jsx
@@ -19,15 +19,12 @@ const VideoChat = () => {
   const handleSubmit = useCallback(async (event) => {
     event.preventDefault();
     try {
-      const roomSID = 'RM212c2f4e1d964fa8741ea26d084b4716';
-      const getToken = await axios.get(`https://a263650b57ce.ngrok.io/api/twilio/token/${roomSID}`);
+      const getToken = await axios.get(`https://a263650b57ce.ngrok.io/api/twilio/token/${roomName}/${username}`);
       console.log(getToken.data.data);
       setToken(getToken.data.data);
     } catch (error) {
       console.log(error.response);
     }
-
-    setToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzIxYjVkZjIyMjU1N2U5NTllYzIyOTYwNzkxYmMxYTRlLTE1OTg0Njg5NDIiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJwYXRpZW50IiwidmlkZW8iOnt9fSwiaWF0IjoxNTk4NDY4OTQyLCJleHAiOjE1OTg0NzYxNDIsImlzcyI6IlNLMjFiNWRmMjIyNTU3ZTk1OWVjMjI5NjA3OTFiYzFhNGUiLCJzdWIiOiJBQzE2YmYyYTNlN2NjYTI3MjhiMzk3NTI1Yzg2ODg1N2MzIn0.gvlyKwEtQV7v6pakBBawhSrHi5gBgen6gIQ0bXf72Kg');
   }, [roomName, username]);
 
   const handleLogout = useCallback(event => {
